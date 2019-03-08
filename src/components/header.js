6fec16dd0f81b28996ca83bc293c489010a8188e
Kang <pkang@pkang-m1v2.activision.com>
2019-03-07 16:55:54 -0800

    updated routing

--- src/components/header.js
+++ src/components/header.js
@@ -2,45 +2,43 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+const ListLink = props =>(
+  <li style={{ marginRight: 20 }}>
+    <Link to={props.to} style={{ textDecoration: `none`, color: `#fff` }}>{props.children}</Link>
+  </li>
+)
+
 const Header = ({ siteTitle }) => (
-  <header
+  <div
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      padding: `1.45rem 1.0875rem`
     }}
   >
-    <div
+    <header
       style={{
-        display: `inline-block`,
-        margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        margin: `0 auto`,
+        padding: `0 1.0875rem`
       }}
     >
-      <h3 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h3>
-      <h3 style={{ margin: 0 }}>
-        <Link 
-          to="/page-2/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
-        >
-          About
-        </Link>
-      </h3>
-    </div>
-  </header>
+      <Link to="/" style={{ color: `white`, textDecoration: `none`, }} >
+        <h3>{siteTitle}</h3>
+      </Link>
+      <ul 
+        style={{ 
+          listStyle: `none`, 
+          display: `flex`, 
+          flexDirection: `row`, 
+          justifyContent: `spaceBetween`, 
+          margin: 0 
+        }}
+      >
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
+  </div>
 )
 
 Header.propTypes = {
--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => (
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener noreferrer">Gatsby</a> and <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>
           </footer>
         </div>
       </>
