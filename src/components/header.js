e49e837fd68514e5188596e7ca0c4718fec4c988
Kang <pkang@pkang-m1v2.activision.com>
2019-04-24 17:24:56 -0700

    update header

--- src/components/header.js
+++ src/components/header.js
@@ -54,7 +54,6 @@ const Header = ({ siteTitle, location }) => (
       >
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/works/">Works</ListLink>
-        <ListLink to="/projects/">Projects</ListLink>
         <ListLink to="/contact/">Contact</ListLink>
       </ul>
     </header>
