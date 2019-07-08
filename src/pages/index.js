5c0fec78af5b868badf1bfa8be9e7b5d823fa36c
Kang <pkang@pkang-m1v2.activision.com>
2019-07-08 15:45:20 -0700

    update welcome page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,26 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1 className="title-container">Hi my name is Pete.</h1>
-    <h1 className="title-container">I'm a developer.</h1>
+
+    <div style={{
+      display: `flex`,
+      justifyContent: `space-between`,
+      alignItems: `center`,
+      height: `80vh`,
+      maxWidth: `440px`,
+      width: `100%`,
+      margin: `0 auto`,
+      padding: `0 20px`,
+    }}>
+      <h1 
+        className="title-container"
+        style={{
+          lineHeight: `1.6`,
+      }}>
+        Hi my name is Pete.<br />
+        I'm a developer.
+      </h1>
+    </div>
   </Layout>
 )
 
